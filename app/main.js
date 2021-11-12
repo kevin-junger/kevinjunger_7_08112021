f05569ae5e26292fb19c3ef4221aab2014b8e46c
Kevin Junger <kevin.junger@outlook.fr>
2021-11-12 17:59:23 +0100

    :lipstick: better ui for fine-search dropdowns and cards

--- app/main.js
+++ app/main.js
@@ -1,8 +1,15 @@
-document.querySelector('#ingredient-input').addEventListener('focusin', () => {
-  document.querySelector('label[for="ingredient-input"]').classList.toggle('visually-hidden')
-  document.querySelector('.ingredient-list').classList.remove('visually-hidden')
-})
-document.querySelector('#ingredient-input').addEventListener('focusout', () => {
-  document.querySelector('label[for="ingredient-input"]').classList.remove('visually-hidden')
-  document.querySelector('.ingredient-list').classList.toggle('visually-hidden')
+// for fine-search dropdowns - temporary
+
+const fine_search_inputs = document.querySelectorAll('.fine-search-input')
+fine_search_inputs.forEach((input) => {
+  input.addEventListener('focusin', () => {
+    input.parentElement.querySelector('.fine-search-label').classList.toggle('visually-hidden')
+    input.setAttribute('placeholder','Rechercher un ingrédient...')
+    input.parentElement.querySelector('.fine-search-list').classList.remove('visually-hidden')
+  })
+  input.addEventListener('focusout', () => {
+    input.parentElement.querySelector('.fine-search-label').classList.remove('visually-hidden')
+    input.setAttribute('placeholder','')
+    input.parentElement.querySelector('.fine-search-list').classList.toggle('visually-hidden')
+  })
 })
