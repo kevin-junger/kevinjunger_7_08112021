d9039bf972d4b63b0f8d380c6f49dc17051b6b9a
Kevin Junger <kevin.junger@outlook.fr>
2021-11-14 15:24:53 +0100

    :lipstick: fine-search dropdowns

--- app/main.js
+++ app/main.js
@@ -3,11 +3,17 @@
 const fine_search_inputs = document.querySelectorAll('.fine-search-input')
 fine_search_inputs.forEach((input) => {
   input.addEventListener('focusin', () => {
+    input.parentElement.classList.toggle('open')
+    input.parentElement.classList.remove('rounded')
+    input.parentElement.classList.toggle('rounded-top')
     input.parentElement.querySelector('.fine-search-label').classList.toggle('visually-hidden')
-    input.setAttribute('placeholder','Rechercher un ingrédient...')
+    input.setAttribute('placeholder','Rechercher...')
     input.parentElement.querySelector('.fine-search-list').classList.remove('visually-hidden')
   })
   input.addEventListener('focusout', () => {
+    input.parentElement.classList.remove('open')
+    input.parentElement.classList.remove('rounded-top')
+    input.parentElement.classList.toggle('rounded')
     input.parentElement.querySelector('.fine-search-label').classList.remove('visually-hidden')
     input.setAttribute('placeholder','')
     input.parentElement.querySelector('.fine-search-list').classList.toggle('visually-hidden')
