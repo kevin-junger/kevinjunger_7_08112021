5d99356650f14c8a83cce6d05719b6436805b77d
Kevin Junger <kevin.junger@outlook.fr>
2021-11-15 12:49:17 +0100

    :lipstick: interface complete

--- app/main.js
+++ app/main.js
@@ -1,21 +1,30 @@
 // for fine-search dropdowns - temporary
 
-const fine_search_inputs = document.querySelectorAll('.fine-search-input')
-fine_search_inputs.forEach((input) => {
-  input.addEventListener('focusin', () => {
-    input.parentElement.classList.toggle('open')
-    input.parentElement.classList.remove('rounded')
-    input.parentElement.classList.toggle('rounded-top')
-    input.parentElement.querySelector('.fine-search-label').classList.toggle('visually-hidden')
-    input.setAttribute('placeholder','Rechercher...')
-    input.parentElement.querySelector('.fine-search-list').classList.remove('visually-hidden')
-  })
-  input.addEventListener('focusout', () => {
-    input.parentElement.classList.remove('open')
-    input.parentElement.classList.remove('rounded-top')
-    input.parentElement.classList.toggle('rounded')
-    input.parentElement.querySelector('.fine-search-label').classList.remove('visually-hidden')
-    input.setAttribute('placeholder','')
-    input.parentElement.querySelector('.fine-search-list').classList.toggle('visually-hidden')
+const fine_search_dropdowns = document.querySelectorAll('.fine-search')
+fine_search_dropdowns.forEach((dropdown) => {
+  dropdown.querySelector('.fine-search-button').addEventListener('click', () => {
+    if(dropdown.classList.contains('open')){
+      closeFineSearchDropdown(dropdown)
+    } else {
+      openFineSearchDropdown(dropdown)
+    }
   })
 })
+
+function openFineSearchDropdown(dropdown) {
+  dropdown.classList.toggle('open')
+  dropdown.classList.remove('rounded')
+  dropdown.classList.toggle('rounded-top')
+  dropdown.querySelector('.fine-search-label').classList.toggle('visually-hidden')
+  dropdown.querySelector('.fine-search-list').classList.remove('visually-hidden')
+  dropdown.querySelector('.fine-search-input').classList.remove('visually-hidden')
+  dropdown.querySelector('.fine-search-input').focus()
+}
+function closeFineSearchDropdown(dropdown) {
+  dropdown.classList.remove('open')
+  dropdown.classList.remove('rounded-top')
+  dropdown.classList.toggle('rounded')
+  dropdown.querySelector('.fine-search-label').classList.remove('visually-hidden')
+  dropdown.querySelector('.fine-search-list').classList.toggle('visually-hidden')
+  dropdown.querySelector('.fine-search-input').classList.toggle('visually-hidden')
+}
