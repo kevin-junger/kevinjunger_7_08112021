0081c4debbf7606b2242f7c66f54447e3a23ffb8
Kevin Junger <kevin.junger@outlook.fr>
2021-11-19 15:15:06 +0100

    🥅 simple error handler

--- app/view/Fail.js
+++ app/view/Fail.js
@@ -1,9 +1,14 @@
 export default class Fail {
   constructor(error) {
     this.error = error
+    this.wrapper = document.querySelector('.wrapper')
   }
 
   init() {
-    console.log(this.error)
+    this.wrapper.innerHTML = `
+      <h2>Oups !</h2>
+      <p>Une erreur s'est produite, veuillez réessayer.</p>
+      <p>${this.error}</p>
+    `
   }
 }
